4e66c451d6a7bded9eca49f3eba8a2ec627184da
Andrea Salucci <asalucci@c2r3p12.42firenze.it>
2026-02-19 11:22:20 +0100

    b

---
+++ o_solo_get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalucci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 16:13:45 by asalucci          #+#    #+#             */
+/*   Updated: 2025/02/19 18:05:54 by asalucci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 80
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s);
+char	*cut_string(char *s, char cut_from_char);
+char	*f_constrn(char *constr);
+char	*get_next_line(int fd);
+#endif
--- parse/parse.c
+++ parse/parse.c
@@ -50,7 +50,57 @@ check_zero_vertically_before_end()
 
 }
 
+int	spot_map_in_file()
+{
+	int		fd;
+	char	*line;
+	int		line_i;
+
+	fd = open();
+	line = get_next_line(fd)
+	line_i = 0;
+	while (line[0] != )
+	{
+		line = get_next_line(fd);
+		line_i++;
+	}
+	return (line_i);
+}
+
+int	count_map_rows()
+{
+	int		fd;
+	char	*line;
+	int		line_n;
+	int		i;
+
+	fd = open();
+	i = 0;
+	while (i < spot_map_in_file())
+		line = get_next_line(fd);
+	line_n = 0;
+	while (line)
+	{
+		line = get_next_line(fd);
+		line_n++;
+	}
+	return (line_n);
+}
+
 create_map_matrix()
 {
-	
+	int		fd;
+	char	*line;
+	char	**map_mat;
+
+	map_mat[0] = line;
+	while (line)
+	{
+		line = get_next_line(fd);
+		map_mat[i] = line;
+	}
+	map_mat[i] = NULL;
 }
+
+
+// Si puo usare get_next_line given that usa una statica?
